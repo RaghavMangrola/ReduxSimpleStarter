3acb24e9e6759f52376c9054d98779f32a68965a
Raghav Mangrola <raghav.mangrola@eventfarm.com>
2017-06-23 08:49:28 -0700

    Refactor App

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
@@ -7,17 +7,26 @@ var config = require('./config')
 
 const API_KEY = config.API_KEY
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-	console.log(data)
-})
-
 // Create a new component. This componenet should produce some HTML
-const App = () => {
-    return (
-			<div>
-				<SearchBar />
-			</div>
-    )
+class App extends Component {
+	
+	constructor(props) {
+		super(props)
+		
+		this.state = { videos: [] }
+		
+		YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+			this.setState({ videos })
+		})
+	}
+	
+	render() {
+		return (
+		<div>
+		<SearchBar />
+		</div>
+		)
+	}
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
