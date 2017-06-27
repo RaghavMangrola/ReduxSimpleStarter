68f73d6669dc83f3a7ed16e18e5860664768a28c
Raghav Mangrola <raghav.mangrola@eventfarm.com>
2017-06-27 09:05:27 -0700

    Search + Style Changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
@@ -20,18 +21,26 @@ class App extends Component {
 						selectedVideo: null
 				}
 
+				this.videoSearch('surfboards')
+		}
+
+		videoSearch(term) {
 				YTSearch({
 						key: API_KEY,
-						term: 'surfboards'
+						term: term
 				}, videos => {
 						this.setState({videos: videos, selectedVideo: videos[0]})
 				})
 		}
 
 		render() {
+				const videoSearch = _.debounce((term) => {
+						this.videoSearch(term)
+				}, 30000)
+
 				return (
 						<div>
-								<SearchBar/>
+								<SearchBar onSearchTermChange={videoSearch}/>
 								<VideoDetail video={this.state.selectedVideo}/>
 								<VideoList
 										onVideoSelect={selectedVideo => this.setState({selectedVideo})}
