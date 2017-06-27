4702ae6412c55565a7e3b479d36e86f1849289e1
Raghav Mangrola <raghav.mangrola@eventfarm.com>
2017-06-27 08:02:16 -0700

    Display Videos + Beautify Code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
@@ -10,26 +10,32 @@ const API_KEY = config.API_KEY
 
 // Create a new component. This componenet should produce some HTML
 class App extends Component {
-	
-	constructor(props) {
-		super(props)
-		
-		this.state = { videos: [] }
-		
-		YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
-			this.setState({ videos })
-		})
-	}
-	
-	render() {
-		return (
-		<div>
-		<SearchBar />
-		<VideoList videos={this.state.videos} />
-		</div>
-		)
-	}
+
+		constructor(props) {
+				super(props)
+
+				this.state = {
+						videos: []
+				}
+
+				YTSearch({
+						key: API_KEY,
+						term: 'surfboards'
+				}, videos => {
+						this.setState({videos})
+				})
+		}
+
+		render() {
+				return (
+						<div>
+								<SearchBar/>
+								<VideoList videos={this.state.videos}/>
+						</div>
+				)
+		}
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'))
+ReactDOM.render(
+		<App/>, document.querySelector('.container'))
